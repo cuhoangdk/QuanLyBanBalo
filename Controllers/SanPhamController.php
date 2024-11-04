40b714fe54ab6216b38b89c811b1b581ebc2b67b
cuhoangdk <104707960+cuhoangdk@users.noreply.github.com>
2024-11-04 10:54:36 +0700

    Update SanPhamController.php

--- Controllers/SanPhamController.php
+++ Controllers/SanPhamController.php
@@ -127,14 +127,53 @@ class SanPhamController
         $stmt->close();
         return $danhMucSanPham;
     }
+    protected function taoMaSanPham($tenSanPham)
+    {
+        // Chuyển tên sản phẩm thành dạng không dấu
+        $tenSanPhamKhongDau = $this->removeAccents($tenSanPham);
+
+        // Tách tên sản phẩm thành từng từ
+        $tu = explode(' ', $tenSanPhamKhongDau);
+
+        // Lấy tối đa 2 ký tự đầu tiên của mỗi từ và ghép lại
+        $maSP = '';
+        foreach ($tu as $t) {
+            // Kiểm tra độ dài của từ, nếu từ có ít hơn 2 ký tự thì lấy tất cả ký tự của từ đó
+            $maSP .= substr($t, 0, min(2, strlen($t)));
+        }
+
+        return strtolower($maSP); // Đảm bảo mã sản phẩm là chữ in thường
+    }
+
+    // Hàm chuyển tiếng Việt có dấu thành không dấu
+    protected function removeAccents($str)
+    {
+        $unwanted_array = [
+            'á'=>'a','à'=>'a','ả'=>'a','ã'=>'a','ạ'=>'a',
+            'ă'=>'a','ắ'=>'a','ằ'=>'a','ẳ'=>'a','ẵ'=>'a','ặ'=>'a',
+            'â'=>'a','ấ'=>'a','ầ'=>'a','ẩ'=>'a','ẫ'=>'a','ậ'=>'a',
+            'é'=>'e','è'=>'e','ẻ'=>'e','ẽ'=>'e','ẹ'=>'e',
+            'ê'=>'e','ế'=>'e','ề'=>'e','ể'=>'e','ễ'=>'e','ệ'=>'e',
+            'í'=>'i','ì'=>'i','ỉ'=>'i','ĩ'=>'i','ị'=>'i',
+            'ó'=>'o','ò'=>'o','ỏ'=>'o','õ'=>'o','ọ'=>'o',
+            'ô'=>'o','ố'=>'o','ồ'=>'o','ổ'=>'o','ỗ'=>'o','ộ'=>'o',
+            'ơ'=>'o','ớ'=>'o','ờ'=>'o','ở'=>'o','ỡ'=>'o','ợ'=>'o',
+            'ú'=>'u','ù'=>'u','ủ'=>'u','ũ'=>'u','ụ'=>'u',
+            'ư'=>'u','ứ'=>'u','ừ'=>'u','ử'=>'u','ữ'=>'u','ự'=>'u',
+            'ý'=>'y','ỳ'=>'y','ỷ'=>'y','ỹ'=>'y','ỵ'=>'y',
+            'đ'=>'d'
+        ];
+        return strtr(mb_strtolower($str), $unwanted_array);
+    }
+
     // Thêm sản phẩm
     public function themSanPham($tenSanPham, $chatLieu, $canNang, $hangSanXuat, $nuocSanXuat, $thoiGianBaoHanh, $gioiThieu, $loai, $doiTuong, $anh, $gia, $soLuong)
     {
-        $sql = "INSERT INTO tdanhmucsp (TenSP, MaChatLieu, CanNang, MaHangSX, MaNuocSX, ThoiGianBaoHanh, GioiThieuSP, MaLoai, MaDT, Anh, Gia, SoLuong)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                
+        $sql = "INSERT INTO tdanhmucsp (MaSP, TenSP, MaChatLieu, CanNang, MaHangSX, MaNuocSX, ThoiGianBaoHanh, GioiThieuSP, MaLoai, MaDT, Anh, Gia, SoLuong)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $maSP = $this->taoMaSanPham($tenSanPham);        
         $stmt = $this->connection->prepare($sql);
-        $stmt->bind_param("ssisssisssii", $tenSanPham, $chatLieu, $canNang, $hangSanXuat, $nuocSanXuat, $thoiGianBaoHanh, $gioiThieu, $loai, $doiTuong, $anh, $gia, $soLuong);
+        $stmt->bind_param("sssdssdssssii", $maSP , $tenSanPham, $chatLieu, $canNang, $hangSanXuat, $nuocSanXuat, $thoiGianBaoHanh, $gioiThieu, $loai, $doiTuong, $anh, $gia, $soLuong);
 
         if ($stmt->execute()) {
             return true;
@@ -166,7 +205,7 @@ class SanPhamController
                 WHERE MaSP = ?";
                 
         $stmt = $this->connection->prepare($sql);
-        $stmt->bind_param("ssisssisssiii", $tenSanPham, $chatLieu, $canNang, $hangSanXuat, $nuocSanXuat, $thoiGianBaoHanh, $gioiThieu, $loai, $doiTuong, $anh, $gia, $soLuong, $maSanPham);
+        $stmt->bind_param("ssdssdsssiis", $tenSanPham, $chatLieu, $canNang, $hangSanXuat, $nuocSanXuat, $thoiGianBaoHanh, $gioiThieu, $loai, $doiTuong, $anh, $gia, $soLuong, $maSanPham);
 
         if ($stmt->execute()) {
             return true;
