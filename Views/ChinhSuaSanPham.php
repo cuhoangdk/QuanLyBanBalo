66a9fa8ee222a6665d83cecb82ddca954938ff92
Kassa <nhokkho18@gmail.com>
2024-11-08 19:49:13 +0700

    update chinh sua san pham

--- Views/ChinhSuaSanPham.php
+++ Views/ChinhSuaSanPham.php
@@ -91,6 +91,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="bg-white shadow-md rounded-lg p-6">
             <h2 class="text-2xl font-bold mb-4">Chỉnh Sửa Sản Phẩm</h2>
             <form action="" method="POST" enctype="multipart/form-data">
+            <div class="mb-2">
+                    <label class="block text-gray-700 font-bold">Mã sản phẩm:</label>
+                    <input type="text" name="tenSanPham" value="<?= htmlspecialchars($sanPham->getMaSanPham()) ?>" disabled
+                        class="w-full px-3 py-2 border rounded-lg">
+                </div>
                 <div class="mb-2">
                     <label class="block text-gray-700 font-bold">Tên sản phẩm:</label>
                     <input type="text" name="tenSanPham" value="<?= htmlspecialchars($sanPham->getTenSanPham()) ?>"
